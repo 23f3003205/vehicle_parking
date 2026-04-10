a77133f896c640f1fdd0d57043e6b0541c8b2c01
23f3003205 <23f3003205@ds.study.iitm.ac.in>
2026-04-11 00:15:16 +0530

    .env file created

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask 
+from flask import Flask
+import os
 from application.database import db        #3
-
+from dotenv import load_dotenv
+load_dotenv()  # loads variables from .env
 
 app=None
 
 def create_app():
     app=Flask(__name__) # creates a web app instance in flask class and figure out necessay files/packages like templates , static
     app.debug=True
-    app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///vehicle_parking.sqlite3' #3  #sets up flask web application  with a SQLIte database using SQLAlchemy
-    app.config["SECRET_KEY"]="session-secret-key"  #its secret that can  not be disclose it used for session creation
+    app.config["SQLALCHEMY_DATABASE_URI"]=  os.getenv("DATABASE_URL") #3  #sets up flask web application  with a SQLIte database using SQLAlchemy
+    app.config["SECRET_KEY"]= os.getenv("SECRET_KEY")  #its secret that can  not be disclose it used for session creation
     db.init_app(app) #connect database object with flask app   #3
     app.app_context().push()# brings down everything in context of flask application , throws runtime error when not applied
     return app
